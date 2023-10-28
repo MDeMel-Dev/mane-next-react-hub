56ba987dec3e305499e5d324a4d6b8d60c7603e9
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-28 20:17:05 +1100

    up add working image link

--- reactComponents/ImageBadge.js
+++ reactComponents/ImageBadge.js
@@ -3,10 +3,10 @@ import '../styles/imagebadge.module.css'
 function ImageBadge() {
     const name = "Mane De Mel";
     const handle = "mdemel-dev";
-    const img = "https://github.com/MDeMel-Dev/mane-next-react-hub/blob/main/resources/mdemel_dev_whizz.png";
+    const img = "https://mdemel-dev.github.io/mane-next-react-hub/resources/mdemel_dev_whizz.png";
   
     return (
-      <div className="badgeImage">
+      <div className="badge">
         <img className="badgeImage" alt={`Badge image: Mane De Mel`} src={img} />
         <div>
           <h4 className="badgeImage">{name}</h4>
